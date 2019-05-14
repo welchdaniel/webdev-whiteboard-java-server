1813638c9ac46bc15d3dca033d9811968f0c1202
dwelch <welch.da@husky.neu.edu>
2019-05-14 17:50:49 -0400

    updated user-admin template

--- src/main/java/com/example/whiteboard/models/User.java
+++ src/main/java/com/example/whiteboard/models/User.java
@@ -48,19 +48,13 @@ public class User {
   }
 
   public User(String username, String password, String firstName, String lastName) {
-    super();
     this.username = username;
     this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
   }
 
-  public User() {
-    super();
-  }
-
   public User(long id, String username, String password, String firstName, String lastName) {
-    super();
     this.id = id;
     this.username = username;
     this.password = password;
