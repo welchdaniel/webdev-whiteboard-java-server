e6d37987f744570dcf0bf3d9a1e875100e659a25
dwelch <welch.da@husky.neu.edu>
2019-05-14 16:50:22 -0400

    added User and UserController classes

--- src/main/java/com/example/whiteboard/controllers/UserController.java
+++ src/main/java/com/example/whiteboard/controllers/UserController.java
@@ -1,4 +1,36 @@
 package com.example.whiteboard.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.whiteboard.models.User;
+
+@RestController
 public class UserController {
+  List<User> usersList = new ArrayList<User>();
+
+  @GetMapping("/users")
+  public List<User> findAllUsers() {
+    return usersList;
+  }
+
+  @DeleteMapping("/users/{userId}")
+  public User[] deleteUser(@PathVariable("userId") int userId) {
+    return null;
+  }
+
+  @PostMapping("/users")
+  public List<User> createUser(@RequestBody User user) {
+    usersList.add(user);
+    return usersList;
+  }
+
+
 }
--- src/main/java/com/example/whiteboard/models/User.java
+++ src/main/java/com/example/whiteboard/models/User.java
@@ -1,4 +1,70 @@
 package com.example.whiteboard.models;
 
 public class User {
+  private long id;
+  private String username;
+  private String password;
+  private String firstName;
+  private String lastName;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public User(String username, String password, String firstName, String lastName) {
+    super();
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public User() {
+    super();
+  }
+
+  public User(long id, String username, String password, String firstName, String lastName) {
+    super();
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
